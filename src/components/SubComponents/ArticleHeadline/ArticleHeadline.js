6d28390fa3a3d3c067a95e50648f900c5d07f391
Thore Caspersen <thorecaspersen@hotmail.com>
2019-06-10 18:41:27 +0200

    fix(article headline): if titel prop is not set, then use children as titel

--- src/components/SubComponents/ArticleHeadline/ArticleHeadline.js
+++ src/components/SubComponents/ArticleHeadline/ArticleHeadline.js
@@ -12,9 +12,9 @@ class ArticleHeadline extends React.Component {
   }
 
   render() {
-    const { titel, font, marginTop } = this.props;
+    const { titel, font, marginTop, children } = this.props;
     return (
-      <h4
+      <h1
         css={css`
           font-family: ${font};
           max-width: 740px;
@@ -24,8 +24,8 @@ class ArticleHeadline extends React.Component {
           margin: ${marginTop} auto 0 auto;
         `}
       >
-        {titel}
-      </h4>
+        {titel || children}
+      </h1>
     );
   }
 }

3a5e7170d658327848dff2db9646ecc0f5136f11
Thore Caspersen <thorecaspersen@hotmail.com>
2019-06-21 18:07:05 +0200

    fix(re-style): insert and change font style for print-note text

--- src/components/SubComponents/PrintNoteText/PrintNoteText.js
+++ src/components/SubComponents/PrintNoteText/PrintNoteText.js
@@ -12,12 +12,18 @@ class PrintNoteText extends React.Component {
   }
 
   render() {
-    const { children, padding } = this.props;
+    const { children, padding, font } = this.props;
 
     return (
       <p
         css={css`
           padding: ${padding};
+          font-family: ${font};
+          letter-spacing: 0.01rem;
+          font-weight: 100;
+          font-style: normal;
+          font-size: 1rem;
+          line-height: 1.2rem;
         `}
       >
         {children}
@@ -27,10 +33,11 @@ class PrintNoteText extends React.Component {
 }
 
 PrintNoteText.propTypes = {
+  font: PropTypes.string,
   children: PropTypes.node,
   padding: PropTypes.string
 };
 
-PrintNoteText.defaultProps = { padding: '10px' };
+PrintNoteText.defaultProps = { padding: '10px', font: 'Arial,sans-serif' };
 
 export default PrintNoteText;
